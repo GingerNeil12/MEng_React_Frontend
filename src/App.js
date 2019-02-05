3fb58bdba9e3d982942b942622203865b5b2c1c4
= <=>
2019-02-05 20:44:10 +0000

    Added the router package and got the basic routes working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
+import Navbar from './components/header/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <div className="container">
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/register" render={props => (
+              <React.Fragment>
+                <Register />
+              </React.Fragment>
+            )
+            }/>
+            <Route exact path="/login" render={
+              props => (
+                <React.Fragment>
+                  <Login />
+                </React.Fragment>
+              )
+            }/>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
