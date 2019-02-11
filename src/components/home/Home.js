cfd35626b4df731887eaf8c95aed47db4722b9e9
= <=>
2019-02-11 11:21:55 +0000

    Added links to the Venn and Euler wiki pages

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Jumbotron, Container, Button, ButtonGroup } from 'react-bootstrap';
 
-export default class Home extends Component {
+export default class Home extends Component {    
     render() {
         return (
             <Container>
@@ -9,8 +9,12 @@ export default class Home extends Component {
                     <h1 className="display-4">Circles!</h1>
                     <p className="lead">We will draw all your Venn and Euler Diagrams</p>
                     <ButtonGroup>
-                        <Button style={ButtonStyle} size="lg" variant="outline-primary">Learn More</Button>
-                        <Button style={ButtonStyle} size="lg" variant="primary">Learn More</Button>
+                        <Button style={ButtonStyle} size="lg" variant="outline-primary">
+                            <a href="https://en.wikipedia.org/wiki/Venn_diagram" target="_" style={LinkStyle}>Go To Venn</a>
+                        </Button>
+                        <Button style={ButtonStyle} size="lg" variant="outline-primary">
+                            <a href="https://en.wikipedia.org/wiki/Euler_diagram" target="_" style={LinkStyle}>Go To Euler</a>
+                        </Button>
                     </ButtonGroup>
                 </Jumbotron>
             </Container>
@@ -24,5 +28,10 @@ const JumbotronStyle = {
 
 const ButtonStyle = {
     margin: "0 .5em"
+}
+
+const LinkStyle = {
+    textDecoration: "none",
+    color: "inherit"
 }
 
