4c3179a09087fbb9fa5b8d91e1e962540d903db8
= <=>
2019-02-12 15:28:04 +0000

    Links appear depending on if user is logged in or not

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { setCurrentUser } from './actions/authActions';
+
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
 import store from './store';
 
 import Home from './components/home/Home';
@@ -11,23 +15,28 @@ import Header from './components/header/Header';
 
 import './App.css';
 
+if (localStorage.jwtToken) {
+    setAuthToken(localStorage.jwtToken);
+    const decoded = jwt_decode(localStorage.jwtToken);
+    store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
-  render() {
-    return (
-      <Provider store={ store }>
-        <Router>
-          <div>
-            <Header />
-            <Route exact path="/" component={Home} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/registerSuccess" component={RegisterSuccess} />
-            <Route exact path="/login" component={Login} />
-          </div>
-        </Router>
-      </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/register" component={Register} />
+                        <Route exact path="/registerSuccess" component={RegisterSuccess} />
+                        <Route exact path="/login" component={Login} />
+                    </div>
+                </Router>
+            </Provider>
+        );
+    }
 }
 
 export default App;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { logoutUser } from '../../actions/authActions';
 
-export default class Header extends Component {
+import PropTypes from 'prop-types';
+
+class Header extends Component {
+
+    onLogoutClick = (e) => {
+        e.preventDefault();
+        this.props.logoutUser();
+    }
+
     render() {
+        const { isAuthenticated, user } = this.props.auth;
+
+        const guestLinks = (
+            <Nav className="ml-auto">
+                <Nav.Link href="/register" style={LinkStyle}>Register</Nav.Link>
+                <Nav.Link href="/login" style={LinkStyle}>Login</Nav.Link>
+            </Nav>
+        )
+
+        const authLinks = (
+            <Nav className="ml-auto">
+                <Nav.Link href="#" style={LinkStyle} onClick={this.onLogoutClick.bind(this)}>
+                    <img
+                        src={user.avatar}
+                        alt={user.name}
+                        style={AvatarStyle}
+                        className="rounded-circle"
+                        title="You must have a Gravatar connected email to display an avatar"
+                    />
+                    Logout
+                </Nav.Link>
+            </Nav>
+        )
+
         return (
             <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
                 <Container>
                     <Navbar.Brand href="/" style={BrandStyle}>
                         Circles
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="toggle-navbar"/>
+                    <Navbar.Toggle aria-controls="toggle-navbar" />
                     <Navbar.Collapse id="toggle-navbar">
-                            <Nav>
-                                <Nav.Link href="/about" style={LinkStyle}>About</Nav.Link>
-                            </Nav>
-                            <Nav className="ml-auto">
-                                <Nav.Link href="/register" style={LinkStyle}>Register</Nav.Link>
-                                <Nav.Link href="/login" style={LinkStyle}>Login</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
+                        <Nav>
+                            <Nav.Link href="/about" style={LinkStyle}>About</Nav.Link>
+                        </Nav>
+                        {isAuthenticated ? authLinks : guestLinks}
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         )
@@ -35,3 +66,18 @@ const BrandStyle = {
     fontSize: "1.4em"
 }
 
+const AvatarStyle = {
+    width: "1.8em",
+    marginRight: "5px"
+}
+
+Header.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, { logoutUser })(Header);
