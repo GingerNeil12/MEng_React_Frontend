d89ec7ecca9a515df952a117768102d69411c97d
= <=>
2019-03-01 01:49:26 +0000

    Diagram Reducer has more options now

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,8 @@
 export const TEST_DISPATCH = 'TEST_DISPATCH';
 export const GET_ERRORS = 'GET_ERRORS';
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
-export const SET_CURRENT_DIAGRAMS = 'SET_CURRENT_DIAGRAMS';
+export const GET_DIAGRAMS = 'GET_DIAGRAMS';
+export const GET_DIAGRAM = 'GET_DIAGRAM';
+export const ADD_DIAGRAM = 'ADD_DIAGRAM';
+export const NEW_DIAGRAM = 'NEW_DIAGRAM';
+export const DIAGRAM_LOADING = 'DIAGRAM_LOADING';
--- src/reducers/diagramReducer.js
+++ src/reducers/diagramReducer.js
@@ -1,16 +1,47 @@
-import { SET_CURRENT_DIAGRAMS } from '../actions/types';
+import {
+    GET_DIAGRAMS,
+    GET_DIAGRAM,
+    ADD_DIAGRAM,
+    NEW_DIAGRAM,
+    DIAGRAM_LOADING
+} from '../actions/types';
 
 const initialState = {
-    diagrams: {}
+    diagrams: [],
+    diagram: {},
+    loading: false
 }
 
-export default function(state = initialState, action){
-    switch(action.payload){
-        case SET_CURRENT_DIAGRAMS:
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case DIAGRAM_LOADING:
             return {
                 ...state,
-                diagrams: action.diagrams
-            }
+                loading: true
+            };
+        case GET_DIAGRAMS:
+            return {
+                ...state,
+                diagrams: action.payload,
+                loading: false
+            };
+        case GET_DIAGRAM:
+            return {
+                ...state,
+                diagram: action.payload,
+                loading: false
+            };
+        case ADD_DIAGRAM:
+            return {
+                ...state,
+                diagrams: [action.payload, ...state.diagrams]
+            };
+        case NEW_DIAGRAM:
+            return {
+                ...state,
+                diagram: action.payload,
+                loading: false
+            };
         default:
             return state;
     }
