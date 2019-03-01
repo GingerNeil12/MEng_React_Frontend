fde1f3e66bf5362f7149b30225ffeb4046a78f08
= <=>
2019-03-01 00:25:44 +0000

    Added the start of the new diagram page

---
+++ src/components/diagram/NewDiagram.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NewDiagram extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default NewDiagram;
