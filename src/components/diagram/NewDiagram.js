59e34aa6a813e1296536dd2a2da0a1b319d302bb
= <=>
2019-03-17 00:01:54 +0000

    Sorted FlexBox for NewDiagram page

--- src/components/diagram/NewDiagram.js
+++ src/components/diagram/NewDiagram.js
@@ -4,17 +4,39 @@ class NewDiagram extends Component {
   render() {
     return (
       <div style={newDiagramConatiner}>
-        <h1>NEW DIAGRAM</h1>
+        <div style={sectionOne}>
+          <p>DIAGRAM CONTROLS</p>
+        </div>
+        <div style={sectionTwo}>
+          <p>DIAGRAM GOES HERE</p>
+        </div>
+        <div style={sectionThree}>
+          <p>VERIFY</p>
+        </div>
       </div>
     )
   }
 }
 
 const newDiagramConatiner = {
-  width: '80%',
+  width: '90%',
   marginRight: 'auto',
   marginLeft: 'auto',
-  marginTop: '2em'
+  marginTop: '2em',
+  display: 'flex',
+  textAlign: 'center'
+}
+
+const sectionOne = {
+  flex: '1'
+}
+
+const sectionTwo = {
+  flex: '3'
+}
+
+const sectionThree = {
+  flex: '1'
 }
 
 export default NewDiagram;
