b0af852266a3259f2a011c954c4050b20b732f8d
= <=>
2019-03-18 02:26:39 +0000

    Added the ShapeUpdate component

--- src/components/diagram/Shape.js
+++ src/components/diagram/Shape.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react'
 import { Button } from 'react-bootstrap'
 
 class Shape extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            show: false
+        }
+
+        this.toggleDiv = this.toggleDiv.bind(this);
+    }
+
+    toggleDiv = () => {
+        const { show } = this.state;
+        this.setState({show : !show })
+    }
   render() {
     return (
       <div>
-        <Button variant="primary" size="sm" style={buttonStyle}>{this.props.shape.name}</Button>
+        <Button variant="primary" size="sm" style={buttonStyle} onClick={this.toggleDiv}>{this.props.shape.name}</Button>
       </div>
     )
   }
