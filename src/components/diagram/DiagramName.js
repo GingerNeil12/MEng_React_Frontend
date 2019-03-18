c42cce47d492ee849b3e0eef17428fa399320b62
= <=>
2019-03-18 01:40:55 +0000

    Added the Diagram Name component

---
+++ src/components/diagram/DiagramName.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class DiagramName extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default DiagramName;
