6dfff8e64b974db9f7a6b7ec26de4aa9de74cd3d
= <=>
2019-03-18 02:09:51 +0000

    Added the DiagramShape component

---
+++ src/components/diagram/DiagramShapes.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class DiagramShapes extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default DiagramShapes
