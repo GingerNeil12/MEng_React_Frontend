bc366165e548091e5b36e84ad758e7f2fdfadeee
= <=>
2019-03-18 01:33:27 +0000

    Added the DiagramControls component

---
+++ src/components/diagram/DiagramControls.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+// This component is for all the controls like updating and saving etc
+
+class DiagramControls extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default DiagramControls;
