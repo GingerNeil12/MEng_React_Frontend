92c6273492c694b00b8035ca75d5cc7673eadd5e
= <=>
2019-03-19 11:11:32 +0000

    Added some content to EditDiagram component

--- src/components/diagram/EditDiagram.js
+++ src/components/diagram/EditDiagram.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react'
+import { getDiagram } from '../../actions/diagramActions'
+import Canvas from './Canvas'
+import EditControls from './EditControls'
 
 class EditDiagram extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div style={formStyle}>
+                <div style={sectionOne}>
+                    <EditControls diagram={this.state.diagram} update={this.update} updateName={this.updateName} />
+                </div>
+                <div style={sectionTwo}>
+                    <Canvas shapes={this.state.diagram.shapes} />
+                </div>
+                <div style={sectionThree}>
+                    <p>Verification</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+const formStyle = {
+    width: '95%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: '1em',
+    display: 'flex',
+    textAlign: 'center'
+}
+
+const sectionOne = {
+    flex: '1'
+}
+
+const sectionTwo = {
+    flex: '3'
+}
+
+const sectionThree = {
+    flex: '1'
 }
 
 export default EditDiagram;
