40a96f12dcdbca18773183354a57dec19d44c8e2
= <=>
2019-03-19 11:07:50 +0000

    Added EditDiagram component

---
+++ src/components/diagram/EditDiagram.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class EditDiagram extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default EditDiagram;
